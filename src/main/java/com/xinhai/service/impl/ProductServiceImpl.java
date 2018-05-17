a745e1f49948858755a2c936e1f16e101a86ef7e
黄官易 <hgy@x.xinhai.com>
2018-05-17 13:53:33 +0800

    新增 新增产品图片管理 接口实现

--- src/main/java/com/xinhai/service/impl/ProductServiceImpl.java
+++ src/main/java/com/xinhai/service/impl/ProductServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.ResourceBundle;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -14,6 +13,7 @@ import com.xinhai.entity.Product;
 import com.xinhai.entity.ProductImg;
 import com.xinhai.entity.ProductType;
 import com.xinhai.service.IProductService;
+import com.xinhai.util.DateUtil;
 import com.xinhai.util.HttpClientUtil;
 import com.xinhai.util.Result;
 
@@ -31,8 +31,8 @@ public class ProductServiceImpl extends BaseResult implements IProductService {
 		params.add(new BasicNameValuePair("type_ico", data.getType_ico()));
 		params.add(new BasicNameValuePair("status", data.getStatus() + ""));
 		params.add(new BasicNameValuePair("sort", data.getSort() + ""));
-		String resultJosn = HttpClientUtil.getPostDefault(url, params);
-		JSONObject jb = JSON.parseObject(resultJosn);
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
 		int code = jb.getIntValue("code");
 		return 0 == code && jb.getIntValue("data") > 0 ? rtnSuccessResult("添加产品分类成功")
 				: rtnFailResult(code == 0 ? Result.ERROR_401 : code, code == 0 ? "添加产品分类失败" : jb.getString("msg"));
@@ -41,9 +41,8 @@ public class ProductServiceImpl extends BaseResult implements IProductService {
 	@Override
 	public Result<List<ProductType>> selProductType() throws Exception {
 		String url = rb.getString("product_type_sel");
-
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new BasicNameValuePair("", ""));
+		// params.add(new BasicNameValuePair("", ""));
 
 		String resultJson = HttpClientUtil.getPostDefault(url, params);
 		JSONObject jb = JSON.parseObject(resultJson);
@@ -55,7 +54,6 @@ public class ProductServiceImpl extends BaseResult implements IProductService {
 	@Override
 	public Result<ProductType> selProductTypeById(String id) throws Exception {
 		String url = rb.getString("product_type_sel_id");
-
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
 		params.add(new BasicNameValuePair("id", id));
 		String resultJson = HttpClientUtil.getPostDefault(url, params);
@@ -76,8 +74,8 @@ public class ProductServiceImpl extends BaseResult implements IProductService {
 		params.add(new BasicNameValuePair("type_ico", data.getType_ico()));
 		params.add(new BasicNameValuePair("status", data.getStatus() + ""));
 		params.add(new BasicNameValuePair("sort", data.getSort() + ""));
-		String resultJosn = HttpClientUtil.getPostDefault(url, params);
-		JSONObject jb = JSON.parseObject(resultJosn);
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
 		int code = jb.getIntValue("code");
 		return 0 == code && jb.getIntValue("data") > 0 ? rtnSuccessResult("修改产品分类成功")
 				: rtnFailResult(code == 0 ? Result.ERROR_401 : code, code == 0 ? "修改产品分类失败" : jb.getString("msg"));
@@ -88,8 +86,8 @@ public class ProductServiceImpl extends BaseResult implements IProductService {
 		String url = rb.getString("product_type_del");
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
 		params.add(new BasicNameValuePair("id", id));
-		String resultJsom = HttpClientUtil.getPostDefault(url, params);
-		JSONObject jb = JSON.parseObject(resultJsom);
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
 		int code = jb.getIntValue("code");
 		return 0 == code && jb.getIntValue("data") > 0 ? rtnSuccessResult("删除产品分类成功")
 				: rtnFailResult(code == 0 ? Result.ERROR_401 : code, code == 0 ? "删除产品分类失败" : jb.getString("msg"));
@@ -97,15 +95,30 @@ public class ProductServiceImpl extends BaseResult implements IProductService {
 
 	@Override
 	public Result<Object> insProduct(Product data) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String url = rb.getString("product_ins");
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("tid", data.getTid() + ""));
+		params.add(new BasicNameValuePair("product_name", data.getProduct_name()));
+		params.add(new BasicNameValuePair("original_price", data.getOriginl_price().toString()));
+		params.add(new BasicNameValuePair("price", data.getPrice().toString()));
+		params.add(new BasicNameValuePair("item_no", data.getItem_no()));
+		params.add(new BasicNameValuePair("info", data.getInfo()));
+		params.add(new BasicNameValuePair("remark", data.getRemark()));
+		params.add(new BasicNameValuePair("add_time", DateUtil.curDateYMDHMS()));
+		params.add(new BasicNameValuePair("status", data.getStatus() + ""));
+		params.add(new BasicNameValuePair("sort", data.getSort() + ""));
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
+		int code = jb.getIntValue("code");
+		return 0 == code && jb.getIntValue("data") > 0 ? rtnSuccessResult("添加产品信息成功")
+				: rtnFailResult(code == 0 ? Result.ERROR_401 : code, code == 0 ? "添加产品信息失败" : jb.getString("msg"));
 	}
 
 	@Override
 	public Result<List<Product>> selProduct() throws Exception {
 		String url = rb.getString("product_sel");
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new BasicNameValuePair("", ""));
+		// params.add(new BasicNameValuePair("", ""));
 		String resultJson = HttpClientUtil.getPostDefault(url, params);
 		JSONObject jb = JSON.parseObject(resultJson);
 		return 0 == jb.getIntValue("code") ? rtnSuccessResult("", JSON.parseArray(jb.getString("data"), Product.class))
@@ -114,33 +127,71 @@ public class ProductServiceImpl extends BaseResult implements IProductService {
 
 	@Override
 	public Result<Product> selProductById(String id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String url = rb.getString("product_sel_id");
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("id", id));
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
+		return 0 == jb.getIntValue("code") ? rtnSuccessResult("", JSON.parseObject(jb.getString("data"), Product.class))
+				: rtnFailResult(jb.getIntValue("code"), jb.getString("msg"));
 	}
 
 	@Override
 	public Result<Object> uptProduct(Product data) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String url = rb.getString("product_upt");
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("id", data.getId() + ""));
+		params.add(new BasicNameValuePair("tid", data.getTid() + ""));
+		params.add(new BasicNameValuePair("product_name", data.getProduct_name()));
+		params.add(new BasicNameValuePair("original_price", data.getOriginl_price().toString()));
+		params.add(new BasicNameValuePair("price", data.getPrice().toString()));
+		params.add(new BasicNameValuePair("item_no", data.getItem_no()));
+		params.add(new BasicNameValuePair("info", data.getInfo()));
+		params.add(new BasicNameValuePair("remark", data.getRemark()));
+		params.add(new BasicNameValuePair("add_time", DateUtil.curDateYMDHMS()));
+		params.add(new BasicNameValuePair("status", data.getStatus() + ""));
+		params.add(new BasicNameValuePair("sort", data.getSort() + ""));
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
+		int code = jb.getIntValue("code");
+		return 0 == code && jb.getIntValue("data") > 0 ? rtnSuccessResult("修改产品信息成功")
+				: rtnFailResult(code == 0 ? Result.ERROR_401 : code, code == 0 ? "修改产品信息失败" : jb.getString("msg"));
 	}
 
 	@Override
 	public Result<Object> delProduct(String id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String url = rb.getString("product_del");
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("id", id));
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
+		int code = jb.getIntValue("code");
+		return 0 == code && jb.getIntValue("data") > 0 ? rtnSuccessResult("删除产品信息成功")
+				: rtnFailResult(code == 0 ? Result.ERROR_401 : code, code == 0 ? "删除产品信息失败" : jb.getString("msg"));
 	}
 
 	@Override
 	public Result<Object> insProductImg(ProductImg data) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String url = rb.getString("product_img_ins");
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("pid", data.getPid() + ""));
+		params.add(new BasicNameValuePair("img_url", data.getImg_url()));
+		params.add(new BasicNameValuePair("title", data.getTitle()));
+		params.add(new BasicNameValuePair("is_main", data.getIs_main() + ""));
+		params.add(new BasicNameValuePair("status", data.getStatus() + ""));
+		params.add(new BasicNameValuePair("sort", data.getSort() + ""));
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
+		int code = jb.getIntValue("code");
+		return 0 == code && jb.getIntValue("data") > 0 ? rtnSuccessResult("添加产品图片成功")
+				: rtnFailResult(code == 0 ? Result.ERROR_401 : code, code == 0 ? "添加产品图片失败" : jb.getString("msg"));
 	}
 
 	@Override
 	public Result<List<ProductImg>> selProductImg() throws Exception {
 		String url = rb.getString("product_img_sel");
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new BasicNameValuePair("", ""));
+		// params.add(new BasicNameValuePair("", ""));
 		String resultJson = HttpClientUtil.getPostDefault(url, params);
 		JSONObject jb = JSON.parseObject(resultJson);
 		return 0 == jb.getIntValue("code")
@@ -150,26 +201,56 @@ public class ProductServiceImpl extends BaseResult implements IProductService {
 
 	@Override
 	public Result<List<ProductImg>> selProductionImgByProId(String proId) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String url = rb.getString("product_img_sel_pId");
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("pid", proId));
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
+		return 0 == jb.getIntValue("code")
+				? rtnSuccessResult("", JSON.parseArray(jb.getString("data"), ProductImg.class))
+				: rtnFailResult(jb.getIntValue("code"), jb.getString("msg"));
 	}
 
 	@Override
 	public Result<ProductImg> selProductImgById(String id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String url = rb.getString("product_img_id");
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("id", id));
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
+		return 0 == jb.getIntValue("code")
+				? rtnSuccessResult("", JSON.parseObject(jb.getString("data"), ProductImg.class))
+				: rtnFailResult(jb.getIntValue("code"), jb.getString("msg"));
 	}
 
 	@Override
 	public Result<Object> uptProductImg(ProductImg data) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String url = rb.getString("product_img_upt");
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("id", data.getId() + ""));
+		params.add(new BasicNameValuePair("pid", data.getPid() + ""));
+		params.add(new BasicNameValuePair("img_url", data.getImg_url()));
+		params.add(new BasicNameValuePair("title", data.getTitle()));
+		params.add(new BasicNameValuePair("is_main", data.getIs_main() + ""));
+		params.add(new BasicNameValuePair("status", data.getStatus() + ""));
+		params.add(new BasicNameValuePair("sort", data.getSort() + ""));
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
+		int code = jb.getIntValue("code");
+		return 0 == code && jb.getIntValue("data") > 0 ? rtnSuccessResult("修改产品图片成功")
+				: rtnFailResult(code == 0 ? Result.ERROR_401 : code, code == 0 ? "修改产品图片失败" : jb.getString("msg"));
 	}
 
 	@Override
 	public Result<Object> delProductImg(String id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String url = rb.getString("product_img_del");
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+		String resultJson = HttpClientUtil.getPostDefault(url, params);
+		JSONObject jb = JSON.parseObject(resultJson);
+		int code = jb.getIntValue("code");
+		return 0 == code && jb.getIntValue("data") > 0 ? rtnSuccessResult("删除产品图片成功")
+				: rtnFailResult(code == 0 ? Result.ERROR_401 : code, code == 0 ? "删除产品图片失败" : jb.getString("msg"));
 	}
 
 }
